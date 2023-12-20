5563d2d284dc45096316303abe91578a68ef3077
Manikanta662001 <gundlurimanikanta142@gmail.com>
2023-12-20 07:11:39 +0530

    first commit

---
+++ mockData.js
@@ -0,0 +1,97 @@
+store = [
+    {
+        "id": 1,
+        "description": "ferrai",
+        "image-url": "https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 2000000,
+        "model": "sx" 
+    },
+    {
+        "id": 2,
+        "description": "mclaren",
+        "image-url": "https://images.unsplash.com/photo-1542362567-b07e54358753?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 1000000,
+        "model": "sports"
+    },
+    {
+        "id": 3,
+        "description": "Dodgecharger",
+        "image-url": "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 40000000,
+        "model": "racer"
+    },
+    {
+        "id": 4,
+        "description": "audi",
+        "image-url": "https://images.unsplash.com/photo-1616422285623-13ff0162193c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=431&q=80",
+        "price": 3000000,
+        "model": "sxx"
+    },
+    {
+        "id": 5,
+        "description": "dodgecharger",
+        "image-url": "https://images.unsplash.com/photo-1522932467653-e48f79727abf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=415&q=80",
+        "price": 100000000,
+        "model": "vintage"
+    },
+    {
+        "id": 6,
+        "description": "yellowferrari",
+        "image-url": "https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+        "price": 7500000,
+        "model": "racer"
+    },
+    {
+        "id": 7,
+        "description": "Greencharger",
+        "image-url": "https://images.unsplash.com/photo-1567818735868-e71b99932e29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+        "price": 15000000,
+        "model": "racer"
+
+    },
+    {
+        "id": 8,
+        "description": "bmw",
+        "image-url": "https://images.unsplash.com/photo-1603386329225-868f9b1ee6c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
+        "price": 25000000,
+        "model": "hatchback"
+    },
+    {
+        "id": 9,
+        "description": "merc",
+        "image-url": "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 150000000,
+        "model": "maybach"
+
+    },
+    {
+        "id": 10,
+        "description": "blackdodge",
+        "image-url": "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 1000000000,
+        "model": "racer"
+    },
+    {
+        "id": 11,
+        "description": "yellowmerc",
+        "image-url": "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+        "price": 8000000,
+        "model": "maybach"
+    },
+    {
+        "id": 12,
+        "description": "lamborg",
+        "image-url": "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=452&q=80",
+        "price": 10000000,
+        "model": "racer"
+    },
+    {
+        "id": 13,
+        "description": "porche",
+        "image-url": "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        "price": 800000000,
+        "model": "coupe"
+    }
+]
+module.exports= {store}
+
---
+++ server.js
@@ -0,0 +1,165 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); //for password hashing
+
+
+
+//used to parse incoming JSON data
+app.use(express.json());
+//used to access data from one domain(url) to another domain
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+// mongoose(library) is used to connect mongodb database.
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Mongodb is connected"))
+  .catch((error) => console.log("Error while connecting your mongodb", error));
+
+//getting data from mockData.js file
+const { store } = require("./mockData");
+// console.log(store)
+
+
+
+//to send email
+const nodemailer = require("nodemailer");
+// Create a transporter with your email service provider's SMTP settings.
+const transporter = nodemailer.createTransport({
+    service: "Gmail", // Use your email service provider (e.g., 'Gmail', 'Outlook')
+    auth: {
+      user: "gundlurimanikanta142@gmail.com", // Your email address
+      pass: "eehf vqhz nuss kdvr", // Your email password or app-specific password
+    },
+  });
+
+
+
+app.get("/carsget", (req, res) => {
+  res.status(200).json(store);
+});
+
+//getting schema
+const { Registermodel } = require("./models/Registermodel");
+
+//register post call
+app.post("/postuserdata", async (req, res) => {
+  try {
+    console.log(req.body, "REGISTERDATA");
+    const { password,email } = req.body;
+
+    let total_items = { ...req.body };
+    let hashed_password = await bcrypt.hash(password, 10);
+    total_items = { ...total_items, password: hashed_password };
+    const dbresponse = await Registermodel.create(total_items)
+      .then((dt) => dt)
+      .catch((err) => err)
+      .finally(() => mongoose.connection.close());
+    console.log(dbresponse, "DBRESPONSE");
+    res.status(200).json(dbresponse);
+
+    // Email content and details
+    const mailOptions = {
+        from: 'gundlurimanikanta142@gmail.com', // Your email address
+        to: email, // Recipient's email address
+        subject: 'Registration', // Email subject
+        text: 'registration is successful', // Email text content
+    };
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error:', error);
+        } else {
+          console.log('Email sent:', info.response);
+        }
+      });
+    
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// login post call
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    console.log(req.body, "LoginDATA");
+    const user = await Registermodel.findOne({ email });
+    console.log(user, "5353");
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    console.log(passwordMatch, "5757");
+    if (passwordMatch) {
+      res.json({ message: "Login successful" });
+      
+    } else {
+      res.status(500).json({ message: "Internal Server error" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// getting data from db
+app.get('/getdbdata',(req,res)=>{
+  let db_data = Registermodel.find({})
+  .then(users=>{
+    console.log(users,"db data") 
+    res.status(200).json(users)
+  })
+  .catch(err=>{
+    console.error(err,"Error while getting data from db")
+    res.json({message:err})
+  })
+  console.log(db_data,"118")
+
+})
+
+app.put("/putupdate/:id",async(req,res)=>{
+  const id = req.params.id
+  console.log(id,"126")
+  // Validate if userId is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    console.log("Invalid ObjectId format");
+    return res.status(400).send("Invalid ObjectId format");
+  }
+  try{
+    let updated =await Registermodel.findByIdAndUpdate(id,req.body,{new:true})
+    console.log(updated,"130")
+    res.status(200).json(updated)
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json("Error when updating the user")
+
+  }
+})
+app.delete("/delete/:id",async(req,res)=>{
+  const id = req.params.id
+  console.log(id,"145")
+  // Validate if userId is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    console.log("Invalid ObjectId format");
+    return res.status(400).send("Invalid ObjectId format");
+  }
+  try{
+    let deleted =await Registermodel.findByIdAndDelete(id)
+    console.log(deleted,"153")
+    res.status(200).json(deleted)
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json("Error when deleting the user")
+
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running in ${process.env.HOST}:${PORT}`);
+});
